1480d3188d3bd71ada25685559099da4c590c65a
Thaydra Ward <62661015+ThaydraW14@users.noreply.github.com>
2020-07-21 18:08:15 -0700

    Create accntHome.java

---
+++ accntHome.java
@@ -0,0 +1,14 @@
+package com.example.beta20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class accntHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accnt_home);
+    }
+}
